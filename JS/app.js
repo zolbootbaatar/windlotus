6ad0eaef27df43bfee38fa0969df0688b6ff7bd7
zolbootbaatar <zolbootbaatar8@gmail.com>
2022-10-26 00:34:05 +0800

    s

--- JS/app.js
+++ JS/app.js
@@ -2,10 +2,4 @@ var button = document.getElementById("l-d");
 var icon = document.getElementById("icon")
 button.onclick = function() {
       document.body.classList.toggle('light-theme');
-      if(document.body.classList.contains('light-theme')) {
-            icon.class = "fa-sharp fa-solid fa-moon";
-      }
-      else {
-            icon.class = "fa-sharp fa-solid fa-sun";
-      }
 }
